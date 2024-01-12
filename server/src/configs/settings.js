ae4eb2df58caf4ab422eb82b038b043ad4db3aaf
George Sheng <gsheng112@gmail.com>
2024-01-12 03:07:03 -0500

    removed js files

--- server/src/configs/settings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoConfig = void 0;
-// You will need to change the DB name to match the required DB name in the assignment specs!
-exports.mongoConfig = {
-    serverUrl: 'mongodb://localhost:27017/',
-    database: 'Knowledge-Base'
-};
--- server/src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFunctionSignature = void 0;
-const getFunctionSignature = (functionName) => {
-    return `[${functionName}]`;
-};
-exports.getFunctionSignature = getFunctionSignature;
